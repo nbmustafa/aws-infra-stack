de68b215aea0b118f63a4264d295cf30bf2d1dfe
nashwan <nashwan.bilal@gmail.com>
2024-11-07 17:00:02 +1100

    creat aws infra

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,3 +1,4 @@
+# Create ssl cert from acm for your hostname
 resource "aws_lb" "web_alb" {
   name               = "web-alb"
   load_balancer_type = "application"
@@ -33,10 +34,11 @@ resource "aws_lb_target_group" "web_tg" {
 
 resource "aws_lb_listener" "web_listener_https" {
   load_balancer_arn = aws_lb.web_alb.arn
-  port              = "443"
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.ssl_cert_arn 
+  certificate_arn   = "placeholder_for_ssl_cert" # Replace with your ACM certificate ARN
+  # certificate_arn   = "arn:aws:acm:us-west-2:123456789012:certificate/your-certificate-id" # Replace with your ACM certificate ARN
 
   default_action {
     type             = "forward"
@@ -46,4 +48,23 @@ resource "aws_lb_listener" "web_listener_https" {
   tags = {
     Name = "WebListenerHTTPS"
   }
+}
+
+resource "aws_lb_listener_rule" "web_listener_rule" {
+  listener_arn = aws_lb_listener.web_listener_https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/*"]
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.web_alb.dns_name
 }
