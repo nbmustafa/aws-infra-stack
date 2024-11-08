719b25f8aba61bde51b3b311dd445b2ba1bc85e4
nashwan <nashwan.bilal@gmail.com>
2024-11-08 16:31:52 +1100

    creat aws infra

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,36 +1,6 @@
+# Creating S3 bucket
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "my-bucket-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  logging {
-    target_bucket = "my-logs-bucket"
-    target_prefix = "log/"
-  }
-
-  lifecycle_rule {
-    id      = "log"
-    enabled = true
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    expiration {
-      days = 365
-    }
-  }
 
   tags = {
     Name        = "My Secure Bucket"
@@ -38,3 +8,45 @@ resource "aws_s3_bucket" "secure_bucket" {
   }
 }
 
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.secure_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption on the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.secure_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Manage lifecycle of objects
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  rule {
+    id     = "transition-to-glacier"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
