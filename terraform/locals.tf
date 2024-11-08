199de825ff500b5c31bced73fba2ef08eac40f82
nashwan <nashwan.bilal@gmail.com>
2024-11-08 21:00:37 +1100

    creat aws infra

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
   organisation        = "dotfuture"
-  corporate_ip_range  = ["0.0.0.0", "0.0.0.0"]
-  cloudflare_ip_range = ["0.0.0.0", "0.0.0.0"]
+  corporate_ip_range  = ["20.0.0.0", "30.0.0.0"]
+  cloudflare_ip_range = ["50.0.0.0", "60.0.0.0"]
 
 }
