6e8aeba81fd37e27de9e1760e125b67390feb959
nashwan <nashwan.bilal@gmail.com>
2024-11-11 11:29:53 +1100

    creat aws infra

---
+++ plan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail  # Enable strict error handling
+
+# Navigate to Terraform directory
+cd terraform/
+
+# Initialize Terraform
+terraform init -backend-config "bucket=<your-bucket>" --backend-config "prefix=/path/to/terraform-statefile/" --reconfigure
+
+# Terraform Plan
+terraform plan
