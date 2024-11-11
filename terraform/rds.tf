b922bd54398ed23abe621bf61232c6a19b1c887d
nashwan <nashwan.bilal@gmail.com>
2024-11-11 11:37:43 +1100

    creat aws infra

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,27 +7,13 @@
 #  }
 #
 
-resource "aws_db_subnet_group" "db_subnet_group_a" {
+resource "aws_db_subnet_group" "main" {
   name       = "${local.organisation}-aurora-db-subnet-group-a"
-  subnet_ids = [aws_subnet.private_db_subnet_a.id]
-
-  tags = {
-    Name = "AuroraDBSubnetGroup"
-  }
-}
-
-resource "aws_db_subnet_group" "db_subnet_group_b" {
-  name       = "${local.organisation}-aurora-db-subnet-group-b"
-  subnet_ids = [aws_subnet.private_db_subnet_b.id]
-
-  tags = {
-    Name = "AuroraDBSubnetGroup"
-  }
-}
-
-resource "aws_db_subnet_group" "db_subnet_group_c" {
-  name       = "${local.organisation}-aurora-db-subnet-group-c"
-  subnet_ids = [aws_subnet.private_db_subnet_c.id]
+  subnet_ids = [
+    aws_subnet.private_db_subnet_a.id,
+    aws_subnet.private_db_subnet_b.id,
+    aws_subnet.private_db_subnet_c.id
+  ]
 
   tags = {
     Name = "AuroraDBSubnetGroup"
